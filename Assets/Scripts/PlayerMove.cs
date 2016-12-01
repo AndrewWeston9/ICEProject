4a5fe06f911f1cbe45130d896ea05590d5fd2aa8
Shaun <incshaun@gmail.com>
2016-12-01 12:55:57 +1100

    Integrated material from blobwort; jump and create bricks.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -37,13 +37,13 @@ public class PlayerMove : NetworkBehaviour
             
             localWorld.placeBlock (playerpos.x, playerpos.z);
 
- /*           Vector3 pos = new Vector3 (px, py + 0.5f, pz);
+            Vector3 pos = new Vector3 (px, py + 0.5f, pz);
             GameObject thisBrick = Object.Instantiate (brick, pos, Quaternion.identity);
                     
-            thisBrick.transform.parent = levelObject.transform;
+//             thisBrick.transform.parent = levelObject.transform;
             y = 0.2f;
             transform.Translate(0, 1, 0);
-   */         
+            
         }
         
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
@@ -59,6 +59,7 @@ public class PlayerMove : NetworkBehaviour
     bool isGrounded()
     {
         var distToGround = 0.4f;
+//         Debug.Log ("distance " + Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f) + " " + transform.position + " - " + (-Vector3.up) + " - " + (distToGround + 0.1f));
         return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
         
